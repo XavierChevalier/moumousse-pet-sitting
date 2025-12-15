4b495552d3b31cd90f645b8a689f37db95e6fb9d
xavierchevalier <chevalierxavier38@gmail.com>
2025-12-15 18:00:49 +0100

    feat: reduce friction by guiding visitors toward a free pre-visit and clarifying contact paths

---
+++ src/config/SiteConfig.ts
@@ -0,0 +1,20 @@
+export const siteConfig = {
+  bookPrevisiteUrl: 'https://calendly.com/steffiethollot/30min',
+
+  social: {
+    // instagram: 'https://instagram.com/...',
+    // facebook: 'https://facebook.com/...',
+    // tiktok: 'https://www.tiktok.com/@...',
+  },
+
+  contact: {
+    encodedPhone: '@HWPjD}PjfGQj\\GQj\\GP',
+    email: 'contact@moumousse-pet-sitting.fr',
+  },
+
+  business: {
+    name: 'Moumousse Pet Sitting',
+    ownerName: 'Steffie Thollot',
+    siret: '994 642 205 00011',
+  },
+} as const

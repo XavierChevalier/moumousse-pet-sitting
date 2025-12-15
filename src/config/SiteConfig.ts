0d168f5080d7c32ff07ae4a11a781d47bcda3ffa
xavierchevalier <chevalierxavier38@gmail.com>
2025-12-15 18:01:54 +0100

    refactor: remove unused social media links from site configuration

--- src/config/SiteConfig.ts
+++ src/config/SiteConfig.ts
@@ -1,12 +1,6 @@
 export const siteConfig = {
   bookPrevisiteUrl: 'https://calendly.com/steffiethollot/30min',
 
-  social: {
-    // instagram: 'https://instagram.com/...',
-    // facebook: 'https://facebook.com/...',
-    // tiktok: 'https://www.tiktok.com/@...',
-  },
-
   contact: {
     encodedPhone: '@HWPjD}PjfGQj\\GQj\\GP',
     email: 'contact@moumousse-pet-sitting.fr',
